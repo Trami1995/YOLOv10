dd16efc71c681b44b4ac9af944d436ea7270d3b7
852598551@qq.com <852598551@qq.com>
2024-06-30 16:20:42 +0800

    fix backbone import error

--- mmyolo/models/backbones/__init__.py
+++ mmyolo/models/backbones/__init__.py
@@ -6,12 +6,10 @@ from .cspnext import CSPNeXt
 from .efficient_rep import YOLOv6CSPBep, YOLOv6EfficientRep
 from .yolov7_backbone import YOLOv7Backbone
 from .yolov10_backbone import YOLOv10Backbone
-from .yolov10_backbone import YOLOv10NanoBackbone, YOLOv10SmallBackbone, YOLOv10MiddleBackbone, YOLOv10LargeBackbone, YOLOv10ExtremeBackbone
+
 
 __all__ = [
     'YOLOv5CSPDarknet', 'BaseBackbone', 'YOLOv6EfficientRep', 'YOLOv6CSPBep',
     'YOLOXCSPDarknet', 'CSPNeXt', 'YOLOv7Backbone', 'PPYOLOECSPResNet',
-    'YOLOv8CSPDarknet', 'YOLOv10SmallBackbone',
-    'YOLOv10MiddleBackbone', 'YOLOv10LargeBackbone', 'YOLOv10ExtremeBackbone', 'YOLOv10NanoBackbone',
-    'YOLOv10Backbone'
+    'YOLOv8CSPDarknet', 'YOLOv10Backbone'
 ]
